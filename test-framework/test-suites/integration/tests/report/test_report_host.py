29bf92f9ee28255ba6bc79970265f3c24e886dda
Ryan Santos <ryan.santos@teradata.com>
2018-07-27 14:40:22 -0700

    BUGFIX: Raise error when reporting on an interface with an IP but no network

--- test-framework/test-suites/integration/tests/report/test_report_host.py
+++ test-framework/test-suites/integration/tests/report/test_report_host.py
@@ -1,4 +1,5 @@
 import pytest
+<<<<<<< HEAD
 import re
 
 @pytest.mark.usefixtures('revert_database')
@@ -150,6 +151,13 @@ class TestReportHost:
 
 		hostsFile.close()
 
-
-
+	@pytest.mark.usefixtures('add_host_with_interface')
+	def test_ip_no_network_fails(self, host):
+		# set interface IP, but not network
+		result = host.run('stack set host interface ip backend-0-0 interface=eth0 ip=1.1.1.1')
+		assert result.rc == 0
+		# invalid configuration; should error
+		result = host.run('stack report host')
+		assert result.rc != 0
+		assert 'has an IP but no network' in result.stderr
 
